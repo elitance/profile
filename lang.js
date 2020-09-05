d187c2c51fa82e0a196fd1800f4f9288c22fe052
Lucas Han <ldhan0715@gmail.com>
2020-09-05 15:23:06 +0900

    Update: Reload on Change

--- lang.js
+++ lang.js
@@ -2,22 +2,21 @@ function changeLang(event) {
     const select = document.querySelector('select');
     const selLang = select.options[select.selectedIndex].value;
     localStorage.setItem('lang',selLang);
+    location.replace(selLang);
 }
 
 function loadLang() {
     const lang = localStorage.getItem('lang');
-    if (lang !== location.href.slice(location.href.lastIndexOf('/') + 1)) {
-        if (!lang || lang === 'en-gb') {
-            localStorage.setItem('lang','en-gb');
-            location.replace('en-gb');
-        } else {
-            location.replace('ko-kr');
-        }
+    if (!lang || lang === 'en-gb') {
+        localStorage.setItem('lang','en-gb');
+        location.replace('en-gb');
+    } else {
+        location.replace('ko-kr');
     }
 }
 
 function init() {
-    loadLang();
+    if (!(location.href.includes('ko-kr') || location.href.includes('en-gb'))) loadLang();
     document.querySelector('select').addEventListener('input',changeLang);
 }
 
